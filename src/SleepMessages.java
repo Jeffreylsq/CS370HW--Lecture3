4b299d3079acb2a7954403265be78ed434f55755
Jeffreylsq <40809623+Jeffreylsq@users.noreply.github.com>
2018-10-01 11:27:58 -0700

    CS370HW--Lecture

---
+++ src/SleepMessages.java
@@ -0,0 +1,43 @@
+
+public class SleepMessages {
+
+	public static void main(String[] args)throws InterruptedException {
+		
+	   
+		
+		 String importantInfo[] = {
+	       "Mares eat oats",
+	       "Does eat oats",
+	       "Little lambs eat ivy",
+	       "A kid will eat ivy too"
+				 
+		 };
+		 for(int i=0; i<importantInfo.length; i++)
+		 {
+			 Thread.sleep(4000);
+			 System.out.println(importantInfo[i]);
+		 }
+	System.out.println("-------------------------------------------------------");
+		 for(int i=0; i<importantInfo.length;i++)
+		 {
+			 try {
+				 Thread.sleep(4000);
+			 }
+			 catch(InterruptedException e)
+			 {
+				 return ;
+			 }
+			 System.out.println(importantInfo[i]);
+		 }
+		 
+		 System.out.println("-------------------------------------------------------");	 
+		
+		 
+		 
+		 
+	}
+		
+
+	}
+
+
